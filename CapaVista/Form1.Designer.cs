5c7a2810740f98453a836000b31d315309185083
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-08-27 02:37:17 -0600

    .

--- CapaVista/Form1.Designer.cs
+++ CapaVista/Form1.Designer.cs
@@ -33,7 +33,7 @@
             this.txtFiltro = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dgvMostrarClientes = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMostrarClientes)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,7 +57,7 @@
             // 
             // txtFiltro
             // 
-            this.txtFiltro.Location = new System.Drawing.Point(476, 359);
+            this.txtFiltro.Location = new System.Drawing.Point(473, 359);
             this.txtFiltro.Name = "txtFiltro";
             this.txtFiltro.Size = new System.Drawing.Size(100, 20);
             this.txtFiltro.TabIndex = 8;
@@ -65,11 +65,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(435, 362);
+            this.label1.Location = new System.Drawing.Point(421, 362);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "Filtro";
+            this.label1.Text = "Filtro ID:";
             // 
             // dgvMostrarClientes
             // 
@@ -79,21 +79,21 @@
             this.dgvMostrarClientes.Size = new System.Drawing.Size(776, 298);
             this.dgvMostrarClientes.TabIndex = 6;
             // 
-            // button1
+            // btnCargar
             // 
-            this.button1.Location = new System.Drawing.Point(218, 356);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Cargar Datos";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCargar.Location = new System.Drawing.Point(218, 356);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 11;
+            this.btnCargar.Text = "Cargar Datos";
+            this.btnCargar.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.txtFiltro);
@@ -114,7 +114,7 @@
         private System.Windows.Forms.TextBox txtFiltro;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvMostrarClientes;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCargar;
     }
 }
 
