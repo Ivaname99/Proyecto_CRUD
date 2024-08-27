0ce28c6f235ca3544b2d4a354b82698702d72dc8
IvElGrimm <126293620+Ivaname99@users.noreply.github.com>
2024-08-27 03:26:19 -0600

    Obtencion de la nueva Cadena de conexion

--- CapaLogica/DataBase.cs
+++ CapaLogica/DataBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace CapaLogica
 {
     public class DataBase
     {
-        public static int ConnectionTimeOut {  get; set; }
+        public static int connectionTimeOut {  get; set; }
         public static string applicationName { get; set; }
 
         public static String ConnectionString
@@ -17,6 +18,12 @@ namespace CapaLogica
             get
             {
                 String cadenaConexion = ConfigurationManager.ConnectionStrings["ProjConnection"].ConnectionString;
+
+                SqlConnectionStringBuilder conexionBuilder = new SqlConnectionStringBuilder(cadenaConexion);
+
+                conexionBuilder.ApplicationName = applicationName ?? conexionBuilder.ApplicationName;
+                conexionBuilder.ConnectTimeout = (connectionTimeOut > 0) ? connectionTimeOut : conexionBuilder.ConnectTimeout;
+                return conexionBuilder.ToString();
             }
         }
     }
